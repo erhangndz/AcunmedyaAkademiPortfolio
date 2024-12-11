593201079d467626e5d2e091f53881e59feb1069
Erhan Gündüz <erhangndz@live.com>
2024-12-11 22:01:38 +0300

    Add project files.

---
+++ AcunmedyaAkademiPortfolio/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunmedyaAkademiPortfolio.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+    }
+}
