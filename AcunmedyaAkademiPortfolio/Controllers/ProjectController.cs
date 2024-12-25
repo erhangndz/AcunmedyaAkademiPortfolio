d968166eeae16e96e5179350fdcd46cce32482ba
Erhan Gündüz <erhangndz@live.com>
2024-12-25 20:53:51 +0300

    updated

--- AcunmedyaAkademiPortfolio/Controllers/ProjectController.cs
+++ AcunmedyaAkademiPortfolio/Controllers/ProjectController.cs
@@ -9,11 +9,42 @@ namespace AcunmedyaAkademiPortfolio.Controllers
 {
     public class ProjectController : Controller
     {
+        //CRUD ==> Create Read
+
         DbAcunmedyaAkademiPortfolioEntities db = new DbAcunmedyaAkademiPortfolioEntities();
         public ActionResult Index()
         {
             var values = db.TblProjects.ToList();
             return View(values);
         }
+
+        [HttpGet]
+        public ActionResult AddProject()
+        {
+            var categoryList = db.TblCategories.ToList();
+
+            List<SelectListItem> categories = (from x in categoryList
+                                               select new SelectListItem
+                                               {
+                                                Text= x.CategoryName,
+                                                Value=x.CategoryId.ToString()
+                                               }).ToList();
+
+            ViewBag.categories = categories;
+
+           
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProject(TblProject model)
+        {
+            //ekleme işlemleri
+
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
